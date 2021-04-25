c9394a6fbae6fdfe0b23ad3a6ef3c3ff76a606ae
George Kruchinina <gkruchin@gmail.com>
2021-04-25 14:47:14 -0400

    onClick events Super Constructor Binding and Arrow Function to avoid 'this'-'bind' issue

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,20 +4,36 @@ class Counter extends Component {
   state = {
     count: 0,
     imageUrl: "https://picsum.photos/200",
-    tags: ["tag1", "tag2", "tag3"],
+    tags: ["tag1", "tag2", "tag3", "tag4"],
+  };
+
+  // COMMENT: you use constructor() and super() to be enssure you can appropriately extend off Component and bind functions
+  // constructor() {
+  //   super();
+  //   console.log("hi");
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+
+  // handleIncrement() {
+  //   console.log("Increment Clicked", this);
+  // }
+
+  // ALTNERATIVELY - INSTEAD of Consturctor/Super/Bind -- You can use the below approach for functions/methods for components
+  // you can use arrow es6 functions to avoid the binding issue -- arrow functions inherit the "this" - they don't rebind it
+  handleIncrement = () => {
+    alert("Sup ph'magh-gyt");
   };
 
   render() {
     return (
       <div>
-        {/* <img src={this.state.imageUrl} alt="" /> */}
-        <span className={this.getBadgeClasses()}>{this.formatCount()} </span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </div>
     );
   }
